cd4a4a44f02a901ada08305002b380a441cca42d
keita1899 <saboten18k@gmail.com>
2024-02-23 17:47:30 +0900

    完了Todo削除機能実装

--- src/components/CompletedTodo.jsx
+++ src/components/CompletedTodo.jsx
@@ -1,10 +1,14 @@
-export const CompletedTodo = ({todo}) => {
+export const CompletedTodo = ({todo, onDeleteCompletedTodo}) => {
+
+  const handleClick = (id) => {
+    onDeleteCompletedTodo(id)
+  }
   
   return (
     <>
       <li className="">
         <span className="todo-text">{todo.text}</span>
-        <button className="delete-completed-todo-button">削除</button>
+        <button className="delete-completed-todo-button" onClick={() => handleClick(todo.id)}>削除</button>
       </li>
     </>
   )
